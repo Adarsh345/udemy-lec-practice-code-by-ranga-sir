f99f90694108d6a98a9ab7691d034809f066a522
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-02-24 23:45:56 +0530

    Implement versioning by the help of uri

---
+++ restful-web-services/src/main/java/com/in23minutes/rest/webservices/restfulwebservices/versioningRestController/PersonController.java
@@ -0,0 +1,23 @@
+package com.in23minutes.rest.webservices.restfulwebservices.versioningRestController;
+
+import com.in23minutes.rest.webservices.restfulwebservices.versioningRestController.model.Name;
+import com.in23minutes.rest.webservices.restfulwebservices.versioningRestController.model.PersonV1;
+import com.in23minutes.rest.webservices.restfulwebservices.versioningRestController.model.PersonV2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path = "/person/")
+public class PersonController {
+    @GetMapping("v1")
+    public PersonV1 getPerson(){
+        return new PersonV1("Bob charli");
+    }
+
+    @GetMapping("v2")
+    public PersonV2 getPersonV2(){
+        return  new PersonV2(new Name( "Adarsh" , "tripathi"));
+    }
+}
