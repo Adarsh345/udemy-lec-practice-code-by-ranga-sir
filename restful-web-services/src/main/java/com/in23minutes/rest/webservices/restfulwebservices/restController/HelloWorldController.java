8070c24648b95e6a6d2f219f225c5a1cd42713b5
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-02-24 22:53:25 +0530

    Learn implementation of internalization.

--- restful-web-services/src/main/java/com/in23minutes/rest/webservices/restfulwebservices/restController/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in23minutes/rest/webservices/restfulwebservices/restController/HelloWorldController.java
@@ -1,13 +1,39 @@
 package com.in23minutes.rest.webservices.restfulwebservices.restController;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+
+    }
+
     @GetMapping(path = "/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "HelloWord";
+    }
+
+    @GetMapping(path = "/hello-world/internalization")
+    public String helloWorldInternalization() {
+        Locale local = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message",local);
+
+        /*i have learn internalization that will give response based on
+        language header for example, if we pass Accept-Language = nl
+        it will return Goedemorgen in the response .
+         en = English (Good morning)
+         nl = Dutch (GoedemorgenO
+         fr = French (Bonjour)
+         de = Deutsch (Gute morgen)
+         */
+
     }
 }
